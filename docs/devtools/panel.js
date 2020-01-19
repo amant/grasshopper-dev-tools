afb824dd52d5aa6e4949e82f7641e367da557259
Aman Tuladhar <aman.tuladhar@gmail.com>
2020-01-19 20:40:18 +0100

    docs: dev tools

---
+++ docs/devtools/panel.js
@@ -0,0 +1,66 @@
+function walk (children = []) {
+  const collection = [];
+
+  const toFilterOut = (node) => {
+    return !(node instanceof HTMLElement) ||
+      (['template', 'custom-style', 'style', 'script'].includes(node.nodeName.toLowerCase()));
+  };
+
+  for (let el of children) {
+    const obj = {
+      name: el.nodeName,
+      children: []
+    };
+
+    if (toFilterOut(el)) {
+      continue;
+    }
+
+    if (el.nodeName.toLowerCase() === 'slot') {
+      obj.children = walk(el.assignedElements());
+
+    } else if (el.shadowRoot) {
+      obj.children = walk(el.shadowRoot.children);
+
+    } else if (el.children && el.children.length > 0) {
+      obj.children = walk(el.children);
+    }
+
+    collection.push(obj);
+  }
+
+  return collection;
+}
+
+function getList(value) {
+  debugger;
+  let list = '<ul>';
+
+  value.forEach(el => {
+    list += `<li>${el.name}</li>`;
+
+    if (el.children && el.children.length > 0) {
+      list += getList(el.children);
+    }
+  });
+
+  list += '</ul>';
+
+  return list;
+}
+
+(function(){
+  document.querySelector('#reload').addEventListener('click', () => {
+    document.querySelector('#app').innerHTML = 'Loading...';
+
+    chrome.devtools.inspectedWindow.eval(
+      `(${walk.toString()}(document.body.children))`, (result, isException) => {
+        if (isException) {
+          console.error('Error reading DOM');
+        } else {
+          document.querySelector('#app').innerHTML = getList(result);
+        }
+      }
+    );
+  })
+}());
